0b6f4f3cf9a10a5a90f17b7e8e300b0fdb0b173f
jacobtheevans <jakegod14@gmail.com>
2019-12-23 19:31:40 +0100

    New game button

---
+++ frontend/src/components/Toolbar/NewGame.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+import { withSocketIo } from '../../SocketIo'
+
+const Button = styled.button`
+  margin-left: auto;
+  display:inline-block;
+  padding: 14px 21px;
+  border: 0.16em solid #FFFFFF;
+  margin: 0 0.3em 0.3em 0;
+  box-sizing: border-box;
+  text-decoration:none;
+  text-transform:uppercase;
+  font-family:'Roboto',sans-serif;
+  font-weight:400;
+  color: #000;
+  text-align:center;
+  transition: all 0.15s;
+
+  &:hover {
+    color: #000;
+    border-color: #DDDDDD;
+  }
+
+  &:active {
+    color: #BBBBBB;
+    border-color: #BBBBBB;
+  }
+
+  @media all and (max-width:30em) {
+    & {
+      display:block;
+      margin:0.4em auto;
+    }
+  }
+`
+
+function newGame (socket) {
+  socket.emit('end_game')
+  window.location.reload()
+}
+
+function NewGame ({ socket }) {
+  return (
+    <Button
+      onClick={() => newGame(socket)}
+    >
+      New Game
+    </Button>
+  )
+}
+
+export default withSocketIo(NewGame)
--- frontend/src/components/Toolbar/index.js
+++ frontend/src/components/Toolbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logout from './Logout'
+import NewGame from './NewGame'
 
 const Bar = styled.div`
   height: 50px;
@@ -10,10 +11,20 @@ const Bar = styled.div`
   flex-direction: row-reverse;
 `
 
+const Username = styled.p`
+  margin: 10px 0px;
+  font-size: 18px;
+  position: absolute;
+  left: 10px;
+  top: 0px;
+`
+
 function Toolbar () {
   return (
     <Bar>
+      <Username>{window.localStorage.getItem('username')}</Username>
       <Logout />
+      <NewGame />
     </Bar>
   )
 }
